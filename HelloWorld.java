4e6ea3f079d3a3c5525f9036b12238f81d2cf953
training <training@PC401941.cts.com>
2019-12-23 12:27:09 +0530

    edhu than second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 ﻿class HelloWorld {
 
 	public static void main(String args[]){
-		System.out.println("Hello From Jenkins");
+		System.out.println("Hello From Jenkinsinmin");
+		for(i=1; i<10; i++)
+		System.out.println("Welcome BhusyFalous");
 	}
 }
